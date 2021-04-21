4d340a394889fb2fa3a223fa349cd31789e6bf88
Marvin <yongfeide123@sina.com>
2021-04-21 21:32:38 +0800

    init

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,7 @@ module.exports = {
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
     'prefer-rest-params': 'off'
   },
 };
